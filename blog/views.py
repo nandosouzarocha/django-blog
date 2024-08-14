d1da9174f4d189bc88f08241ddc05f307b1912db
Nando Rocha <nando.souza.rocha@gmail.com>
2024-08-14 20:42:29 +0100

    Added dummy data directly in the views.py file to simulate a more realistic scenario. Made the changes needed in index.html and post.html, so the data could be shown.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,87 @@
+from datetime import date
 from django.shortcuts import render
 
 # Create your views here.
 
+def get_date(post):
+    return post['date']
+
+all_posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "Bernhard Riemann",
+        "date": date(2024, 8, 13),
+        "title": "Mountain Hiking",
+        "excerpt": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        "content": """
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+        """
+    },
+    {
+        "slug": "coding-can-be-really-fun",
+        "image": "coding.jpg",
+        "author": "Bernhard Riemann",
+        "date": date(2024, 8, 3),
+        "title": "Coding can be really fun!",
+        "excerpt": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        "content": """
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+        """
+    },
+    {
+        "slug": "a-weekend-in-the-woods",
+        "image": "woods.jpg",
+        "author": "Bernhard Riemann",
+        "date": date(2024, 8, 14),
+        "title": "A weekend in the woods.",
+        "excerpt": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        "content": """
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
+        """
+    },
+]
+
 
 def starting_page(request):
-    return render(request, 'blog/index.html')
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, 'blog/index.html',{
+        'posts': latest_posts,
+    })
 
 
 def posts(request):
