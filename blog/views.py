47140eff47d36894cef3eb693a7c877531825367
Nando Rocha <nando.souza.rocha@gmail.com>
2024-08-29 15:57:09 +0100

    I created a PostAdmin class in the admin.py file and registered it. This class is responsible to change the visibility, the way the data is shown, at the admin panel; the data, I mean, regarding the posts. I also changed views.py, so it can be possible to retrieve data directly from the database, instead of the static data previously put in this file.

--- blog/views.py
+++ blog/views.py
@@ -1,84 +1,17 @@
 from datetime import date
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def get_date(post):
     return post['date']
 
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Bernhard Riemann",
-        "date": date(2024, 8, 13),
-        "title": "Mountain Hiking",
-        "excerpt": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-        "content": """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-        """
-    },
-    {
-        "slug": "coding-can-be-really-fun",
-        "image": "coding.jpg",
-        "author": "Bernhard Riemann",
-        "date": date(2024, 8, 3),
-        "title": "Coding can be really fun!",
-        "excerpt": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-        "content": """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-        """
-    },
-    {
-        "slug": "a-weekend-in-the-woods",
-        "image": "woods.jpg",
-        "author": "Bernhard Riemann",
-        "date": date(2024, 8, 14),
-        "title": "A weekend in the woods.",
-        "excerpt": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-        "content": """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat similique in deserunt earum maiores esse aperiam vero et possimus, est, nostrum expedita asperiores inventore atque molestias ea aliquid odit! Aperiam?
-        """
-    },
-]
+all_posts = []
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3] # Taking only the 3 latest posts
     return render(request, 'blog/index.html',{
         'posts': latest_posts,
     })
